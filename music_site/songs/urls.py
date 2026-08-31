86491cf99bf657160aa4476722becd0315679ed1
Gzy907002 <3639939671@qq.com>
2026-09-01 04:14:28 +0800

    搜索页面完成

--- music_site/songs/urls.py
+++ music_site/songs/urls.py
@@ -5,5 +5,8 @@ from . import views
 app_name = "songs"
 
 urlpatterns = [path("", views.song_list, name="song_list"),
-               path("songs/<str:song_id>/", views.song_detail, name="song_detail")
-               ]
+               path("songs/<str:song_id>/", views.song_detail, name="song_detail"),
+               path("artists/", views.singer_list, name="artists"),
+               path("artists/<str:artist_id>/", views.artist_detail, name="artist_detail"),
+               path("search/", views.search_view,name="search"),
+            ]
--- music_site/songs/views.py
+++ music_site/songs/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Song
+from .models import Song, Artist
 from django.core.paginator import Paginator
+from django.db.models import Q
 # Create your views here.
 
 def song_list(request):
@@ -13,3 +14,46 @@ def song_list(request):
 def song_detail(request, song_id):
     song = get_object_or_404(Song.objects.select_related("artist"), song_id=song_id)
     return render(request, 'songs/song_detail.html', {'song': song})
+
+def singer_list(request):
+    singer_queryset = Artist.objects.all()
+    paginator = Paginator(singer_queryset, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'songs/artist_list.html', {'page_obj': page_obj})
+
+def artist_detail(request, artist_id):
+    artist = get_object_or_404(Artist, artist_id=artist_id)
+    songs = Song.objects.filter(artist=artist)
+    return render(request, 'songs/artist_detail.html', {'artist': artist, 'songs': songs})
+
+def search_view(request):
+    keyword = request.GET.get("q", "").strip()
+    search_type = request.GET.get("type", "song")
+
+    if search_type not in ["song", "artist"]:
+        search_type = "song"
+
+    if search_type == "song":
+        queryset = Song.objects.select_related("artist").filter(
+            name__icontains=keyword
+        )
+    else:
+        queryset = Artist.objects.filter(
+            name__icontains=keyword
+        )
+
+    paginator = Paginator(queryset, 10)
+
+    page_number = request.GET.get("page", 1)
+    page_obj = paginator.get_page(page_number)
+
+    return render(
+        request,
+        "songs/search.html",
+        {
+            "keyword": keyword,
+            "search_type": search_type,
+            "page_obj": page_obj,
+        }
+    )
